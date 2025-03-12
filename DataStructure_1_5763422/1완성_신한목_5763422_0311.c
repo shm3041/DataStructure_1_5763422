1176cf7f306f1bf9b83079e20aba19d2b400440b
shm3041 <shm3041@naver.com>
2025-03-13 00:50:26 +0900

    2025.03.11_DataStructure_First_Project1

--- DataStructure_1_5763422/1완성_신한목_5763422_0311.c
+++ DataStructure_1_5763422/1완성_신한목_5763422_0311.c
@@ -9,13 +9,14 @@ int main() {
 	scanf_s("%d", &input);
 
 	//10진수 -> 2진수로 변환하여 출력하기
+	 //32자리만큼 출력, 왼쪽부터 출력하기 위해 오른쪽으로
+	 //비트를 이동시키고, AND연산자를 사용하여 해당 비트에
+	 //값이 존재한다면 1, 존재하지 않는다면 0을 출력
 	printf("입력된 정수의 32비트 표현 : ");
-	for (int i = 31; i >= 0; i--) {
+	for (int i = 31; i >= 0; i--) { 
 		result = input >> i & 1;
 		printf("%d", result);
 	}
-
-	printf("dd %h", input);
 
 	return 0;
 }
