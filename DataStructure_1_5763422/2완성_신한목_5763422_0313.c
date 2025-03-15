0708a590460283aa64ca516625ebb66714d24381
shm3041 <shm3041@naver.com>
2025-03-15 17:51:39 +0900

    project 2, 3 upload

---
+++ DataStructure_1_5763422/2완성_신한목_5763422_0313.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+
+int main() {
+    float input; //소수 입력받을 변수
+    unsigned int bits; //소수를 정수로 변환하여 받을 변수
+
+    //소수 입력받고 자료형 변환
+    printf("소수를 입력하세요 : ");
+    scanf_s("%f", &input);
+    bits = *(unsigned int*)&input; //비트 연산에 사용하기 위해 float -> unsigned int로 변환
+
+    //1비트(부호부) 출력
+    printf("부호 (1비트) : %d\n", bits >> 31 & 1);
+
+    //8비트(지수부) 출력
+    printf("지수 (8비트) : ");
+    for (int i = 30; i >= 23; i--) {
+        printf("%d ", bits >> i & 1);
+    }
+    printf("\n");
+
+    //23비트(가수부) 출력
+    printf("가수 (23비트) : ");
+    for (int i = 22; i >= 0; i--) {
+        printf("%d ", (bits >> i) & 1);
+    }
+    printf("\n");
+
+    return 0;
+}
---
+++ DataStructure_1_5763422/3완성_신한목_5763422_0313.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <string.h> //strlen()
+
+int main() {
+	char input[100]; //입력된 문자열 저장할 배열
+
+	//문자열 입력
+	printf("문자열을 입력하세요: ");
+	gets(input);
+
+	//입력된 문자열 길이 출력
+	printf("문자열 길이: %d\n", strlen(input));
+
+	for (int i = 0; i < strlen(input); i++) {
+		//문자열이 대문자라면 소문자로
+		if (input[i] >= 'A' && input[i] <= 'Z') {
+			input[i] = input[i] + 32;
+		}
+		//문자열이 소문자라면 대문자로
+		else if (input[i] >= 'a' && input[i] <= 'z') {
+			input[i] = input[i] - 32;
+		}
+	}
+	//변환된 문자열 출력
+	printf("변환된 문자열 : %s", input);
+
+	return 0;
+}
