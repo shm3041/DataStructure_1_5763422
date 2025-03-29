0d5d508b571d8ebd92f76b69e0dc3013862c441d
shm3041 <shm3041@naver.com>
2025-03-29 22:37:47 +0900

    UPDATE project 10, 11

--- DataStructure_1_5763422/10완성_신한목_5763422_0327.c
+++ DataStructure_1_5763422/10완성_신한목_5763422_0327.c
@@ -5,16 +5,19 @@ int main() {
 	int** arr1;
 	int** arr2;
 
+	//2차원 동적 메모리 할당 arr1
 	arr1 = (int**)malloc(sizeof(int*) * 2);
 	for (int i = 0; i < 2; i++) {
 		arr1[i] = (int*)malloc(sizeof(int) * 2);
 	}
 
+	//2차원 동적 메모리 할당 arr2
 	arr2 = (int**)malloc(sizeof(int*) * 2);
 	for (int i = 0; i < 2; i++) {
 		arr2[i] = (int*)malloc(sizeof(int) * 2);
 	}
 
+	//첫 번째 행렬 입력
 	printf("첫 번째 행렬 입력:\n");
 	for (int i = 0; i < 2; i++) {
 		for (int j = 0; j < 2; j++) {
@@ -22,6 +25,7 @@ int main() {
 		}
 	}
 
+	//두 번째 행렬 입력
 	printf("두 번째 행렬 입력:\n");
 	for (int i = 0; i < 2; i++) {
 		for (int j = 0; j < 2; j++) {
@@ -29,15 +33,25 @@ int main() {
 		}
 	}
 
+	//행렬 덧셈 결과 출력
 	printf("행렬 덧셈 결과:\n");
 	for (int i = 0; i < 2; i++) {
-		for (int j = 0; j < 1; j++) {
-			printf("%d %d\n", arr1[i][j] + arr2[i][j], arr1[i][j + 1] + arr2[i][j + 1]);
+		for (int j = 0; j < 2; j++) {
+			printf("%d ", arr1[i][j] + arr2[i][j]);
 		}
+		printf("\n");
 	}
 
-
+	//동적 메모리 할당 해제 arr1
+	for (int i = 0; i < 2; i++) {
+		free(arr1[i]);
+	}
 	free(arr1);
+
+	//동적 메모리 할당 해제 arr2
+	for (int i = 0; i < 2; i++) {
+		free(arr2[i]);
+	}
 	free(arr2);
 
 	return 0;
--- DataStructure_1_5763422/11완성_신한목_5763422_0327.c
+++ DataStructure_1_5763422/11완성_신한목_5763422_0327.c
@@ -2,31 +2,41 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+//학생 구조체 선언
 typedef struct student {
-    char name[20];
-    int stnum, score;
+    char name[20]; //이름
+    int stnum, score; //학번, 점수
 } StType;
 
 int main() {
-    int size;
-    StType* students;
-    int stnum;
-    bool found;
+    int size; //students 동적 메모리 할당 크기
+    StType* students; //학생 구조체
+    int stnum; //학번
+    bool found; //학번 존재여부 확인 (1: true, 0: false)
 
+    //학생 수 입력받기
     printf("몇 명의 학생 정보를 입력하시나요? ");
     scanf_s("%d", &size);
     students = (StType*)malloc(sizeof(StType) * size);
 
+    //학생 정보 입력받기 (이름, 학번, 점수)
     for (int i = 0; i < size; i++) {
         printf("[%d] 학생의 정보를 입력하세요 (이름 학번 점수): ", i);
         scanf_s("%s %d %d", students[i].name, sizeof(students[i].name), &students[i].stnum, &students[i].score);
     }
 
+    //학번으로 학생정보 검색
     while (1) {
         found = false;
+
+        //학번 입력받기
         printf("학번을 입력하세요 (0이면 종료): ");
         scanf_s("%d", &stnum);
+
+        //0입력 시 종료
         if (stnum == 0) break;
+
+        //학생 정보 찾기
         for (int i = 0; i < size; i++) {
             if (stnum == students[i].stnum) {
                 printf("%s %d %d\n", students[i].name, students[i].stnum, students[i].score);
@@ -34,9 +44,12 @@ int main() {
                 break;
             }
         }
+
+        //학생 정보가 없다면 출력
         if (!found) printf("해당 학번의 학생이 없습니다.\n");
     }
 
+    //동적 메모리 할당 해제
     free(students);
     return 0;
 }
