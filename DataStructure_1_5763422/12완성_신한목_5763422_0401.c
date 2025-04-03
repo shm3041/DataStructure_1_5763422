53a16db9e7b64c078f8b511d44dafbf005e5704d
shm3041 <shm3041@naver.com>
2025-04-03 21:36:19 +0900

    UPDATE project 12

--- DataStructure_1_5763422/12완성_신한목_5763422_0401.c
+++ DataStructure_1_5763422/12완성_신한목_5763422_0401.c
@@ -1,23 +1,28 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int fibo_rec(int n) {
+unsigned long long int call = 0;
+
+unsigned long long int fibo_rec(int n) {
 	if (n == 0) return 0;
 	else if (n == 1) return 1;
 	else {
+		call++;
 		return (fibo_rec(n - 1) + fibo_rec(n - 2));
 	}
 }
 
-int fibo_itr(int n) {
-	int f_n_2 = 0;
-	int f_n_1 = 1;
-	int f_n = 0;
+unsigned long long int fibo_itr(int n) {
+	unsigned long long int f_n_2 = 0;
+	unsigned long long int f_n_1 = 1;
+	unsigned long long int f_n = 0;
 
 	if (n == 0) return 0;
 	else if (n == 1) return 1;
 
 	for (int i = 2; i <= n; i++) {
+		call++;
+
 		f_n = f_n_1 + f_n_2;
 		f_n_2 = f_n_1;
 		f_n_1 = f_n;
@@ -37,13 +42,17 @@ int main() {
 			printf("정수 N을 입력하세요 (종료하려면 0 입력): ");
 			scanf("%d", &n);
 
-			printf("F (%d) = %d (재귀적 방법)\n", n, fibo_rec(n));
+			call = 0;
+			printf("F (%d) = %llu (재귀적 방법)\n", n, fibo_rec(n));
+			printf("호출횟수: %llu\n", call);
 			break;
 		case 2:
 			printf("정수 N을 입력하세요 (종료하려면 0 입력): ");
 			scanf("%d", &n);
 
-			printf("F (%d) = %d (순환적 방법)\n", n, fibo_itr(n));
+			call = 0;
+			printf("F (%d) = %llu (순환적 방법)\n", n, fibo_itr(n));
+			printf("호출횟수: %llu\n", call);
 			break;
 		case 0:
 			printf("프로그램이 종료되었습니다.\n");
